99e2725cc75d1510f3e3c20a6a44a5170da39741
substack <substack@bits.coop>
2021-12-07 12:01:07 -1000

    tracking nested

---
+++ nested.js
@@ -0,0 +1,33 @@
+var igc = require('intersect-great-circle')
+var v0 = [0,0], v1 = [0,0], v2 = [0,0], v3 = [0,0], v4 = [0,0]
+
+module.exports = function geoPointInPolygon(point, P, start, end, far) {
+  if (Array.isArray(start)) {
+    far = start
+    start = 0
+  } else if (Array.isArray(end)) {
+    far = end
+    end = P.length
+  }
+  var inside = false
+  if (start === undefined) start = 0
+  if (end === undefined) end = P.length
+  if (far === undefined) {
+    v4[0] = (((P[start][0]+360)%360)+360)%360-180
+    v4[1] = -P[start][1]
+    far = v4
+  }
+  v2[0] = point[0]
+  v2[1] = point[1]
+  var len = end - start
+  for (var i = 0, j = len - 1; i < len; j = i++) {
+    v0[0] = P[i+start][0]
+    v0[1] = P[i+start][1]
+    v1[0] = P[j+start][0]
+    v1[1] = P[j+start][1]
+    if (igc(v3,v0,v1,v2,far)) {
+      inside = !inside
+    }
+  }
+  return inside
+}
